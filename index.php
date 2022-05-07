10e7c530403ada14be7e77ec9e78ccb015251a0c
Thiago Alves <thiago1.4.7@outlook.com>
2022-05-07 19:30:26 -0300

    Montando a estrutura

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-
 <!doctype html>
 <html ⚡>
 <head>
   <title>Webjump | Backend Test | Dashboard</title>
   <meta charset="utf-8">
 
-<link  rel="stylesheet" type="text/css"  media="all" href="css/style.css" />
+<link  rel="stylesheet" type="text/css"  media="all" href="assets/css/style.css" />
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet">
 <meta name="viewport" content="width=device-width,minimum-scale=1">
 <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
@@ -16,21 +15,21 @@
 <amp-sidebar id="sidebar" class="sample-sidebar" layout="nodisplay" side="left">
   <div class="close-menu">
     <a on="tap:sidebar.toggle">
-      <img src="images/bt-close.png" alt="Close Menu" width="24" height="24" />
+      <img src="assets/images/bt-close.png" alt="Close Menu" width="24" height="24" />
     </a>
   </div>
-  <a href="dashboard.html"><img src="images/menu-go-jumpers.png" alt="Welcome" width="200" height="43" /></a>
+  <a href="index.php"><img src="assets/images/menu-go-jumpers.png" alt="Welcome" width="200" height="43" /></a>
   <div>
     <ul>
-      <li><a href="categories.html" class="link-menu">Categorias</a></li>
-      <li><a href="products.html" class="link-menu">Produtos</a></li>
+      <li><a href="assets/pages/categories.php" class="link-menu">Categorias</a></li>
+      <li><a href="assets/pages/products.php" class="link-menu">Produtos</a></li>
     </ul>
   </div>
 </amp-sidebar>
 <header>
   <div class="go-menu">
     <a on="tap:sidebar.toggle">☰</a>
-    <a href="dashboard.html" class="link-logo"><img src="images/go-logo.png" alt="Welcome" width="69" height="430" /></a>
+    <a href="index.php" class="link-logo"><img src="assets/images/go-logo.png" alt="Welcome" width="69" height="430" /></a>
   </div>
   <div class="right-box">
     <span class="go-title">Administration Panel</span>
@@ -43,12 +42,12 @@
       <h1 class="title">Dashboard</h1>
     </div>
     <div class="infor">
-      You have 4 products added on this store: <a href="addProduct.html" class="btn-action">Add new Product</a>
+      You have 4 products added on this store: <a href="assets/pages/addProduct.php" class="btn-action">Add new Product</a>
     </div>
     <ul class="product-list">
       <li>
         <div class="product-image">
-          <img src="images/product/tenis-runner-bolt.png" layout="responsive" width="164" height="145" alt="Tênis Runner Bolt" />
+          <img src="assets/images/product/tenis-runner-bolt.png" layout="responsive" width="164" height="145" alt="Tênis Runner Bolt" />
         </div>
         <div class="product-info">
           <div class="product-name"><span>Tênis Runner Bolt</span></div>
@@ -58,7 +57,7 @@
       <li>
         <div class="product-image">
           <a href="tenis-basket-light.html" title="Tênis Basket Light">
-            <img src="images/product/tenis-basket-light.png" layout="responsive" width="164" height="145" alt="Tênis Basket Light" />
+            <img src="assets/images/product/tenis-basket-light.png" layout="responsive" width="164" height="145" alt="Tênis Basket Light" />
           </a>
         </div>
         <div class="product-info">
@@ -69,7 +68,7 @@
       <li>
         <div class="product-image">
           <a href="tenis-basket-light.html" title="Tênis Basket Light">
-           <img src="images/product/tenis-2d-shoes.png" layout="responsive" width="164" height="145" alt="Tênis 2D Shoes" />
+           <img src="assets/images/product/tenis-2d-shoes.png" layout="responsive" width="164" height="145" alt="Tênis 2D Shoes" />
           </a>
         </div>
         <div class="product-info">
@@ -79,7 +78,7 @@
       </li>
       <li>
         <div class="product-image">
-          <img src="images/product/tenis-sneakers-43n.png" layout="responsive" width="164" height="145" alt="Tênis Sneakers 43N" />
+          <img src="assets/images/product/tenis-sneakers-43n.png" layout="responsive" width="164" height="145" alt="Tênis Sneakers 43N" />
         </div>
         <div class="product-info">
           <div class="product-name"><span>Tênis Sneakers 43N</span></div>
@@ -93,7 +92,7 @@
   <!-- Footer -->
 <footer>
 	<div class="footer-image">
-	  <img src="images/go-jumpers.png" width="119" height="26" alt="Go Jumpers" />
+	  <img src="assets/images/go-jumpers.png" width="119" height="26" alt="Go Jumpers" />
 	</div>
 	<div class="email-content">
 	  <span>go@jumpers.com.br</span>
